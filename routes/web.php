62fd267e0826afe43e4ee0f86d1c4d72d1652edf
Ricardo1435 <ricantog1435@gmail.com>
2022-07-21 08:36:01 -0600

    Se configura vista principal, se crean los controladores y se hacen las migraciones de la BD

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,43 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+
+})->name('home');
+
+Route::prefix('/category')->group(
+    function (){
+
+        //Rutas de vistas
+        Route::get('/', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categoryIndex');
+
+        Route::get('/register', [\App\Http\Controllers\CategoryController::class, 'register'])->name('categoryRegister');
+
+        Route::get('/edit/{id}', [\App\Http\Controllers\CategoryController::class, 'edit'])->name('categoryEdit');
+
+        //Rutas http, peticiones al server
+        Route::post('/create', [\App\Http\Controllers\CategoryController::class, 'create'])->name('categoryCreate');
+        Route::patch('/update/{id}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categoryUpdate');
+        Route::delete('/delete/{id}', [\App\Http\Controllers\CategoryController::class, 'delete'])->name('categoryDelete');
+    }
+);
+
+
+Route::prefix('/customer')->group(
+    function (){
+
+        //Rutas de vistas
+        Route::get('/', [\App\Http\Controllers\CustomerController::class, 'index'])->name('customerIndex');
+
+        Route::get('/register', [\App\Http\Controllers\CustomerController::class, 'register'])->name('customerRegister');
+
+        Route::get('/edit/{id}',[\App\Http\Controllers\CustomerController::class, 'edit'])->name('customerEdit');
+
+
+        //Rutas http, peticiones al server
+        Route::post('/create', [\App\Http\Controllers\CustomerController::class, 'create'])->name('customerCreate');
+        Route::patch('/update/{id}', [\App\Http\Controllers\CustomerController::class, 'update'])->name('customerUpdate');
+        Route::delete('/delete/{id}', [\App\Http\Controllers\CustomerController::class, 'delete'])->name('customerDelete');
+
+    }
+);
+
